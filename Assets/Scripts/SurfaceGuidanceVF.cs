24742581f67c6cada89f760157eec640b539581e
alberto-rota <alberto_rota@outlook.com>
2022-07-17 14:02:19 +0200

    Added 'Global' static class for universal props and methods

--- Assets/Scripts/SurfaceGuidanceVF.cs
+++ Assets/Scripts/SurfaceGuidanceVF.cs
@@ -103,8 +103,8 @@ public class SurfaceGuidanceVF : MonoBehaviour
             surface.GetComponent<MeshRenderer>().material = colorred;
         }
         if (graphics) {
-            Arrow(EndEffector.position, closestP, Color.red);
-            Arrow(EndEffector.position, EndEffector.position+force*graphicVectorGain, Color.blue);
+            Global.Arrow(EndEffector.position, closestP, Color.red);
+            Global.Arrow(EndEffector.position, EndEffector.position+force*graphicVectorGain, Color.blue);
         }
 
         // DRAWING MESH NORMALS [DEPRECATED]
