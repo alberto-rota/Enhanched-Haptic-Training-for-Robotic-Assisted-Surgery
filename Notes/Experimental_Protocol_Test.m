14f8f427462efb9f10cc0ddead13ccdd5d06ac7f
Alberto Rota <alberto_rota@outlook.com>
2022-08-09 10:22:04 +0200

    Folder Cleanup

---
+++ Notes/Experimental_Protocol_Test.m
@@ -0,0 +1,28 @@
+close all; clear; interpreterlatex;
+r = 0:25;
+p = 1-exp(-0.2*r)+0.05*randn(1,numel(r)); 
+p(p>=1)=1; p(p<=0)=0;
+dp = [0,diff(p)];
+wfigure;
+subplot(211); 
+yyaxis left;
+plot(r,p,'-o',LineWidth=1.2,MarkerSize=2.5);
+grid on; hold on; 
+xlabel("Repetition"); ylabel("Performance"); 
+ylim([-0.5,1.5]);
+a = 1-p.^(0.99);
+yyaxis right;  ylabel("Assistance"); 
+ylim([-0.5,1.5]);
+plot(r,a,'-o',LineWidth=1.2,MarkerSize=2.5);
+subplot(212); 
+yyaxis left;
+plot(r,dp,'-o',LineWidth=1.2,MarkerSize=2.5);
+a = 1-p.^(0.99) -dp;
+a(a<0)=0;
+grid on; hold on; 
+xlabel("Repetition"); ylabel("$\delta$Performance");
+ylim([-0.5,1.5]);
+yyaxis right
+plot(r,a,'-o',LineWidth=1.2,MarkerSize=2.5);
+ylabel("Assistance");
+ylim([-0.5,1.5]);
