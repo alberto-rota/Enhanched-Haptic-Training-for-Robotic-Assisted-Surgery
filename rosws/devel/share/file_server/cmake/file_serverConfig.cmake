f1b0f01fc1a624ba87caca1acc573b358800f3dd
alberto-rota <alberto_rota@outlook.com>
2022-11-28 16:39:32 +0100

    Repackaged ROSWS

--- rosws/devel/share/file_server/cmake/file_serverConfig.cmake
+++ rosws/devel/share/file_server/cmake/file_serverConfig.cmake
@@ -154,7 +154,7 @@ foreach(library ${libraries})
     set(lib_path "")
     set(lib "${library}-NOTFOUND")
     # since the path where the library is found is returned we have to iterate over the paths manually
-    foreach(path /mnt/c/Users/alber/Desktop/Active_Constraints/rosws/devel/lib;/mnt/c/Users/alber/Desktop/Active_Constraints/rosws/devel/lib;/opt/ros/noetic/lib)
+    foreach(path /mnt/c/Users/alber/Desktop/Active_Constraints/rosws/devel/lib;/home/alberto_rota/ambf/build/devel/lib;/mnt/c/Users/alber/Desktop/Active_Constraints/rosws/devel/lib;/opt/ros/noetic/lib)
       find_library(lib ${library}
         PATHS ${path}
         NO_DEFAULT_PATH NO_CMAKE_FIND_ROOT_PATH)
